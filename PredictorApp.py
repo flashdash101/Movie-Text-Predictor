4f5c74ec46010d5d8020721933115a020f8092f1
Emmanuel A <97402685+flashdash101@users.noreply.github.com>
2023-09-14 17:21:08 +0100

    Massive Performance boost, uses caching.
    Massive feature changes. Uses caching now to store previous results for fast retrieval , tweaked accuracy to improve accuracy. Overall more efficient!

--- PredictorApp.py
+++ PredictorApp.py
@@ -8,7 +8,13 @@ import time
 last_update_time = time.time()
 from fuzzywuzzy import fuzz, process
 import threading
+from functools import lru_cache
 #Initialize the global variables
+prediction_thread = None
+
+
+
+
 
 last_update_time = time.time()
 global key_press_timer
@@ -50,9 +56,14 @@ scrollbar.config(command=predictions_listbox.yview)
 # Lock for thread synchronization
 thread_lock = threading.Lock()
 
+
+@lru_cache(maxsize=128, typed=True)
+#Define a cache function to cache predictions, optimizing for smoother user experience.
+
 #Uses a thread to make predictions
 #Optimizes for smoother user experience
 def make_pred_threaded(user_input, callback):
+    print('Running make_pred_threaded')
     def make_predictions():
         with thread_lock:
             predictions = make_pred(user_input, 5)  # Adjust the number of predictions as needed
@@ -62,8 +73,19 @@ def make_pred_threaded(user_input, callback):
     thread.start()
 
 
+def clear_cache_periodically(interval_seconds):
+    while True:
+        time.sleep(interval_seconds)
+        make_pred_threaded.cache_clear()
 
-def make_pred(text, n_words, beam_width=3, similarity_threshold=100):
+# Start a thread to clear the cache every 5 seconds 
+cache_clear_thread = threading.Thread(target=clear_cache_periodically, args=(15,))
+cache_clear_thread.daemon = True  # Set the thread as a daemon to stop when the main program exits
+cache_clear_thread.start()
+
+
+
+def make_pred(text, n_words, beam_width=3, similarity_threshold=90):
     final_predictions = set()  # Initialize a set to store final predictions
     added_predictions = set()  # Keep track of added predictions to avoid duplicates
 
@@ -125,33 +147,46 @@ def make_pred(text, n_words, beam_width=3, similarity_threshold=100):
 
 def update_predictions(event):
     global key_press_timer
+    global prediction_thread
 
     user_input = entry.get()
     
     if not user_input:
         predictions_listbox.delete(0, END)
-        print('Detected no user inut')
+        print('Detected no user input')
         print('Deleting fields')
         return
 
     if key_press_timer is not None:
         # Cancel the previous timer if it exists
-        
         root.after_cancel(key_press_timer)
+
+    if prediction_thread is not None:
+        # Cancel the previous prediction thread if it exists
+        prediction_thread.cancel()
+
+    
     
     # Set a new timer to call update_predictions after a delay (e.g., 500 ms)
-    key_press_timer = root.after(400, make_predictions_threaded, user_input)
+    key_press_timer = root.after(150, make_predictions_threaded, user_input)
 
-
+    
 
 def make_predictions_threaded(user_input):
     # Clear previous predictions
     predictions_listbox.delete(0, END)
 
     # Utilize threading to make predictions
-    make_pred_threaded(user_input, update_ui)
+    global prediction_thread
+    prediction_thread = threading.Timer(0.125,make_pred_threaded, args=(user_input, update_ui))
+    #Additional debounce to prevent the user from making too many predictions at a time.
+    prediction_thread.start()
+    # Run the function in the main thread and update the UI
+    print('Making predictions')
 
 def update_ui(predictions):
+    predictions_listbox.delete(0, END)
+    print('Updating UI')
     # Run the function in the main thread and update the UI
     for prediction in predictions:
         predictions_listbox.insert(END, prediction)
